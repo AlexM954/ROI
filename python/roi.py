0a3c0046fb9f36bdfba3ec82f47aa226f3b3b7ca
AlexM954 <alexmommers@gmail.com>
2024-10-22 15:30:38 +0200

    changed roi variable to t, and added roi.new function.

--- python/roi.py
+++ python/roi.py
@@ -1,12 +1,23 @@
 # Implementation of the ROI algorithm in Python.
 
+from copy import deepcopy
+
 import numpy as np
 
 
-roi = {
-    "x": [],
-    "y": [],
+t = {
+    "x": None,
+    "y": None,
     "mean": 0,
     "done": False,
     "extended": False
 }
+
+
+def new(x, y):
+    roi = deepcopy(t)
+    roi["x"] = np.asarray([x])
+    roi["y"] = np.asarray([y])
+    roi["mean"] = x
+
+    return roi
